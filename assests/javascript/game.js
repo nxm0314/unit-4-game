9874970cae2435a22ab18090f3f818ace3270de8
Nancy Ta <nxm0314@icloud.com>
2020-01-15 13:30:29 -0500

    Finish collecting the images, now working on the CSS and html and dabbling in JavaScript. Priority: finish the html and CSS.

---
+++ assests/javascript/game.js
@@ -0,0 +1,14 @@
+var targetNumber = 100;
+
+
+$('#number-to-guess').text(targetNumber);
+
+
+var counter = 0;
+
+
+var numberOptions = [2, 5, 1, 3, 4]
+
+for (var i = 0; i < numberOptions.length; i++) {
+    var green = $('g');
+}
