d4960b7c3fade769d991b100e5e7617af8918441
Nancy Ta <nxm0314@icloud.com>
2020-01-16 21:47:04 -0500

    making this live.

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,14 +1,1 @@
-var targetNumber = 100;
-
-
-$('#number-to-guess').text(targetNumber);
-
-
-var counter = 0;
-
-
-var numberOptions = [2, 5, 1, 3]
-
-for (var i = 0; i < numberOptions.length; i++) {
-    var crystal = $('c');
-}
+$(document).ready() 
