21def87a44dda8546d56229ac8df3e41bfa40bbc
Nancy Ta <nxm0314@icloud.com>
2020-01-20 22:30:15 -0500

    work with a tutor to help get the JavaScript flowing. Understanding JavaScript a little better.

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,1 +1,73 @@
-$(document).ready() 
+$(document).ready(function(){
+
+    var randomNumber = Math.floor(Math.random() * 100)
+    // console.log(randomNumber)
+
+
+    var computerNumber = document.getElementById('randomNumber');
+
+    computerNumber.textContent = " " + randomNumber
+
+    var crystal = document.getElementById('crystalCollected');
+    crystal.textContent = "0" + collected;
+
+    // remember to do the wins and loses element.
+
+    var crystal1 = 0;
+    var crysta2 = 0;
+    var crystal3 = 0;
+    var crystal4 = 0;
+
+
+    $('#one').on('click',function(i) {
+
+        
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
