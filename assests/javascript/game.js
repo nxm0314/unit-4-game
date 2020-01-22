aea3b034b3b1438356a05fd4ee1af4ad6ed85258
Nancy Ta <nxm0314@icloud.com>
2020-01-21 23:09:00 -0500

    took a tutoring lesson , and read some chapters on Javascript and ended up trying it out with the help of the tutor. It seem to work but can't get the number to pop up on the screen. Getting ahang of JavaScript a little better.

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,73 +1,99 @@
-$(document).ready(function(){
 
-    var randomNumber = Math.floor(Math.random() * 100)
-    // console.log(randomNumber)
+$( document ).ready(function(){
+    // Generates random number to guess
+    var Random=Math.floor(Math.random() * 102 +1 9)
+    
+    // Display random number
+    $('#scoreToMatch').text(Random);
+    
+    //Generate random number for each crystal
+    var crystal1= Math.floor(Math.random() * 12 + 1)
+    var crystal2= Math.floor(Math.random() * 12 + 1)
+    var crystal3= Math.floor(Math.random() * 12 + 1)
+    var crystal4= Math.floor(Math.random() * 12 + 1)
+    
+    // Variables to keep track of wins, losses and total
+    var playerTotal= 0; 
+    var wins= 0;
+    var losses = 0;
+    
+  
+  $('#wins').text(wins);
+  $('#losses').text(losses);
+  
+  // Reset game
+  function reset(){
+        Random=Math.floor(Math.random() * 102 + 19);
+        console.log(Random)
+
+        $('#scoreToMatch').text(Random);
+        crystal1= Math.floor(Math.random()*12+1);
+        crystal2= Math.floor(Math.random()*12+1);
+        crystal3= Math.floor(Math.random()*12+1);
+        crystal4= Math.floor(Math.random()*12+1);
+
+        playerTotal= 0;
+
+        $('#totalScore').text(playerTotal);
+        } 
+  
 
 
-    var computerNumber = document.getElementById('randomNumber');
+  function woohoo(){
+  alert("Congrats! You won!");
+    wins++; 
+    $('#wins').text(wins);
+    reset();
+  }
 
-    computerNumber.textContent = " " + randomNumber
-
-    var crystal = document.getElementById('crystalCollected');
-    crystal.textContent = "0" + collected;
-
-    // remember to do the wins and loses element.
-
-    var crystal1 = 0;
-    var crysta2 = 0;
-    var crystal3 = 0;
-    var crystal4 = 0;
-
-
-    $('#one').on('click',function(i) {
-
+  function loser(){
+  alert ("Sorry! You lose!");
+    losses++;
+    $('#losses').text(losses);
+    reset()
+  }
+  // Clicking crystals
+    $('#one').on('click', function(){
+      playerTotal = playerTotal + crystal1;
+      $('#collected').text(playerTotal); 
+    
+          if (playerTotal == Random){
+            woohoo();
+          }
+          else if ( playerTotal > Random){
+            loser();
+          }   
+    })  
+    $('#two').on('click', function(){
+      playerTotal = playerTotal + crystal2;
+      $('#collected').text(playerTotal); 
+          if (playerTotal == Random){
+            woohoo();
+          }
+          else if ( playerTotal > Random){
+            loser();
+          } 
+    })  
+    $('#three').on('click', function(){
+      playerTotal = playerTotal + crystal3;
+      $('#collected').text(playerTotal);
+  
+            if (playerTotal == Random){
+            woohoo();
+          }
+          else if ( playerTotal > Random){
+            loser();
+          } 
+    })  
+    $('#four').on ('click', function(){
+      playerTotal = playerTotal + crystal4;
+      $('#collected').text(playerTotal); 
         
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+            if (playerTotal == Random){
+            woohoo();
+          }
+          else if ( playerTotal > Random){
+            loser();
+          }
+    });   
+  }); 
